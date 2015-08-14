55612af50712aecee22584b5e8b717f6f7fac593
YuZakuro <zakuro@yuzakuro.me>
2015-08-15 00:17:20 +0900

    And serve task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,6 +3,7 @@
 var gulp = require('gulp')
 
 var babel = require('gulp-babel')
+var gls = require('gulp-live-server')
 var sourcemaps = require('gulp-sourcemaps')
 
 
@@ -13,5 +14,14 @@ gulp.task('es6', () =>
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('dist'))
 )
+
+gulp.task('serve', function() {
+  var server = gls.new('app.js')
+  server.start()
+  gulp.watch('src/**/*.js', () => { 
+    console.log('Restarting server...')
+    server.start.call(server) 
+  })
+})
 
 gulp.task('default', ['es6'])
