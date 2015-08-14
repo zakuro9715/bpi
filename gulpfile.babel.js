4521e3412f1fbf60cc66460d159a98cbf6bf2aed
YuZakuro <zakuro@yuzakuro.me>
2015-08-14 23:26:11 +0900

    Rewrite with es6

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,5 +2,16 @@
 
 var gulp = require('gulp')
 
+var babel = require('gulp-babel')
+var sourcemaps = require('gulp-sourcemaps')
 
-gulp.task('default', () => {})
+
+gulp.task('es6', () =>
+  gulp.src('src/**/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(babel())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist'))
+)
+
+gulp.task('default', ['es6'])
