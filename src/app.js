a14219dd0b9d29b89131638d2a0bdf4aabd83c32
YuZakuro <zakuro@yuzakuro.me>
2015-08-15 01:35:33 +0900

    Add staticfile serving

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser')
 
+app.use(express.static('dist/static'));
 app.use(bodyParser.json());
 
 app.use('/compile', require('./routes/compile'))
