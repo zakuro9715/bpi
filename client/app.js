7ec3f53a13ee909f0d0ced153c564ae5ce24f53b
YuZakuro <zakuro@yuzakuro.me>
2015-08-15 05:31:36 +0900

    Implement realtime compile textarea

--- client/app.js
+++ client/app.js
@@ -4,5 +4,26 @@ var vm = new Vue({
   el: '#app',
   data: {
     html: '',
+    bHtml: '',
+  },
+  methods: {
+    compile: function(html) {
+      var req = new XMLHttpRequest()
+      var self = this;
+      req.onreadystatechange = () => {
+        if(req.readyState != 4)
+          return  // haven't completed
+        if(req.status == 200)
+          this.html = JSON.parse(req.response).html
+        else
+          this.html = 'Error!'
+      }
+      req.open('POST', '/compile')
+      req.setRequestHeader('Content-Type', 'application/json')
+      req.send(JSON.stringify({'b-html': this.bHtml}))
+    }
+  },
+  watch: {
+    bHtml: 'compile',
   },
 })
